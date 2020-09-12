3808c4492bdec4ccd25c4e97f5f4aa8c90806894
Akshay Vijay Jain <akshayj3@hexaware.com>
2020-09-12 17:09:27 +0530

    adding main.js

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Main = () => {
+    return <div className="parent">
+        <div className="header"></div>
+        <div className="sidebar">
+            <div/>
+            <div/>
+            <div/>
+        </div>
+        <div className="content"></div>
+    </div>
+}
